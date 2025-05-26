8d4e5e8472dd88b83054331c4ba522881d7b699a
Ha Nguyen Ngoc <98647965+HaNguyen1099@users.noreply.github.com>
2025-05-26 22:48:42 +0700

    feat/telegram

--- app/database.py
+++ app/database.py
@@ -1,6 +1,9 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+
+load_dotenv()
 
 POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
